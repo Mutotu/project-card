73bf73364ecd596c640991d87ae56e9cd9f5c4d6
mutotu <muttalip.aydemir@gmail.com>
2022-04-26 15:51:48 +0100

    getting components to work

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,26 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const DUMMY_PRODUCTS = [
+  { id: "p1", price: 6, title: "MY first book", description: "OLOLOLOLO" },
+  { id: "p2", price: 3, title: "MY second book", description: "OLOLOLOLO" },
+];
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={[product.description]}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
